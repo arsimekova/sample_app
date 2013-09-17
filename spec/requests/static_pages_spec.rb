dc19f2a15102ac921c05b0fd6c8d847899e9b0fa
arsimekova <arsimekova@gmail.com>
2013-09-17 13:22:26 +0300

    Contact page added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	let(:base_title) {"Sample App"}
   describe "Home page" do
-    it "should have content 'Sample App'" do
+    it "should have title 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
     visit '/static_pages/home'
     page.should have_selector('title',
-    	:text => "Sample App | Home")
+    	:text => "#{base_title} | Home")
     end
   end
 
@@ -18,11 +19,21 @@ describe "Static pages" do
     end
   end
 
+  describe "Contact" do
+    it "should have title 'Contact'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    visit '/static_pages/contact'
+    page.should have_selector('title',
+    	:text => "#{base_title} | Contact")
+    end
+  end
+
   describe "About page" do
-    it "should have content 'About us'" do
+    it "should have content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
     visit '/static_pages/about'
-    page.should have_content('About us')
+    page.should have_selector('title',
+    	:text => "#{base_title} | About")
     end
   end
 
