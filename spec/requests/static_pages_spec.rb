8f6ee829216ede9e086212bc16f6d7baf22d939e
arsimekova <arsimekova@gmail.com>
2013-09-17 16:36:51 +0300

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Static pages" do
 	let(:base_title) {"Sample App"}
   describe "Home page" do
-    it "should have title 'Sample App'" do
+    it "should have title 'Home'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
     visit '/static_pages/home'
     page.should have_selector('title',
