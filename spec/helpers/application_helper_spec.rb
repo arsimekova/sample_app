7123530b89a252fb44c49cac8d0904e59db02199
arsimekova <arsimekova@gmail.com>
2013-09-18 13:59:24 +0300

    Tests updated

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+  describe "full_title" do
+    it "should include the page title" do
+      full_title("Sample App").should =~ /Sample App/
+    end
+
+    it "should include the base title" do
+      full_title("Home").should =~ /^Sample App/
+    end
+
+    it "should not include a bar for the home page" do
+      full_title("").should_not =~ /\|/
+    end
+  end
+end
