d8de4e8152abf61809877440158d619b84edb3d6
arsimekova <arsimekova@gmail.com>
2013-10-07 18:55:35 +0300

    Config updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ SampleApp::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'heroku.com'}
+  config.action_mailer.default_url_options = { :host => 'sheltered-tor-3590.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :adress => "smtp.gmail.com",
