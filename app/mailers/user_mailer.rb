158af8e3d07244b6e815d9ec4ada30beaaeffaf5
arsimekova <arsimekova@gmail.com>
2013-11-25 14:15:22 +0300

    Updated mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: "NoReply"
+  default from: "app19083991@heroku.com"
 
   def welcome_email(user)
   	@user = user
   	@url = "<a href='http://localhost:3000/sign_in'>http://localhost:3000/sign_in/</a>"
-  	@site_name = "localhost"
+  	@site_name = "Sample_App"
   	mail(:to => user.email, :subject => "Welcome to my website")
   	
   end
